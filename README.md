1cf9e5708f77a10e0779bb6faf2d74721588fcf0
Sze Ning Mak <mak@Szes-MBP.fios-router.home>
2020-06-08 23:30:03 -0400

    update

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # TyingRule_SUnSOnBranching
 
-This set of codes is dedicated to calculating the SU(n) to SO(n) branching rules via a set of graphical rules called "tying rules" as introduced in the work by S. J. Gates, Jr., Yangrui Hu and S. N. Hazel Mak (arXiv:2005.xxxxx). 
+This set of codes is dedicated to calculating the SU(n) to SO(n) branching rules via a set of graphical rules called "tying rules" as introduced in the work by S. J. Gates, Jr., Yangrui Hu and S. N. Hazel Mak ([arXiv:2006.03609](https://arxiv.org/abs/2006.03609)). 
 
 
 ## Installation
 
-Download the [LieART](https://lieart.hepforge.org/) Mathematica application written by Robert Feger, Thomas W. Kephart, and Robert J. Saskowski (see [arXiv:1206.6379](https://arxiv.org/abs/1206.6379) and [arXiv:1912.10969](https://arxiv.org/abs/1912.10969)). Also download the TyingRule.m file in the repository. 
+Download the [LieART](https://lieart.hepforge.org/) Mathematica application written by Robert Feger, Thomas W. Kephart, and Robert J. Saskowski. Also download the TyingRule.m file in the repository. 
 
 In Mathematica, to install these two packages, click "File -> Install...", choose the options "Type of Item to Install: Package" and "Source: From File...", and choose the corresponding files. Click "OK" to finish installation. 
 
 To call the packages in a Mathematica notebook, type
 
-```bash
+```
 << LieART`
 << TyingRule`
 ```
@@ -25,50 +25,58 @@ Note that the LieART package must be called first.
 
 First, input a SU(n) irreducible representation as a Young Tableau shape. The format would be a list of non-strictly decreasing positive integers, where the i-th entry indicates the number of boxes in the i-th column of the Young Tableau. 
 
-Another information needed is the maximum number of rows in the SO(n) algebra, which we call m.
+Another information needed is the maximum number of rows in the SO(n) algebra, which we call rowmaxno.
 
-```bash
+```
 YTshape = {5,3,2};
-m = 5;
+rowmaxno = 5;
 ```
 
 To decompose the SU(n) irreducible representation represented by the above Young Tableau shape into a list of SO(n) irreducible representations also in Young Tableau shapes,
 
-```bash
-TyingRule[YTshape, m]
+```
+TyingRule[YTshape, rowmaxno]
+```
+
+One can visualize the Young Tableau shapes in list of integers by using the following function to draw the actual Young Tableau diagrams.
+
+```
+TyingDisplayYT[YTshape,rowmaxno]
 ```
 
 To convert them into irreducible representations of B_m = SO(2m+1), 
 
-```bash
-TyingDisplayAllB[YTshape, m]
+```
+TyingDisplayAllB[YTshape, rowmaxno]
 ```
 
 or D_m = SO(2m), 
 
-```bash
-TyingDisplayAllB[YTshape, m]
+```
+TyingDisplayAllB[YTshape, rowmaxno]
 ```
 
+where rowmaxno = m for both cases.
 
 ### Dimension Checks for Branching Rules
 
 To check the tying rules give the correct decomposition of SU(n) irreducible representation in SO(n), a metric is the matching of dimensions. The following function works for SU(2m+1) to SO(2m+1) branching rules and it returns the Boolean value "True" if the dimensions match up.
 
-```bash
-BranchingA2nBnDimCheck[YTshapes, m]
+```
+BranchingA2nBnDimCheck[YTshapes, rowmaxno]
 ```
 
 A similar function below works for SU(2m) to SO(2m) branching rules.
 
-```bash
-BranchingA2n1DnDimCheck[YTshapes, m]
+```
+BranchingA2n1DnDimCheck[YTshapes, rowmaxno]
 ```
 
 To check that this condition satisfies for all of the Young Tableau shapes with less than or equal to 3 columns, first generate a list that contains all of them,
 
-```bash
-allYTshapes = GenerateAllYTShapes[m, 3];
+```
+colmaxno = 3;
+allYTshapes = GenerateAllYTShapes[rowmaxno, colmaxno];
 ```
 
 Then run the following lines and see if they return "True".
@@ -81,7 +89,24 @@ And[##] & @@ Array[BranchingA2n1DnDimCheck[allYTshapes[[#]], rowmaxno] &, Length
 
 ## Remarks
 
-1. Tying rules work for Young Tableaux with less than or equal to three columns. 
+1. Tying rules work for Young Tableaux with less than or equal to three columns.
 
 2. In these codes, we perform the calculations of SU(n) to SO(n) branching rules for n > 6 cases.
+
+
+## Relevant arXiv Papers
+
+This little package was originally written by S. N. Hazel Mak for the paper [arXiv:2006.03609](https://arxiv.org/abs/2006.03609), which is the third paper in a series of research papers on the counting aspect of 10D and 11D superfields by S. J. Gates, Jr., Yangrui Hu and S. N. Hazel Mak. Interested readers can go to the following links.
+
+1. 10D superfields: [arXiv:1911.00807](https://arxiv.org/abs/1911.00807)
+2. 11D superfields: [arXiv:2002.08502](https://arxiv.org/abs/2002.08502)
+3. Techniques: [arXiv:2006.03609](https://arxiv.org/abs/2006.03609)
+
+For the LieART package written by Robert Feger, Thomas W. Kephart, and Robert J. Saskowski, one can go to the following arXiv links for complete user manuals. 
+
+1. LieART: [arXiv:1206.6379](https://arxiv.org/abs/1206.6379)
+2. LieART 2.0: [arXiv:1912.10969](https://arxiv.org/abs/1912.10969)
+
+
+
 
--- TyingRule.m
+++ TyingRule.m
@@ -1,250 +1,222 @@
 (* ::Package:: *)
 
-(* ::Subsection:: *)
-(*Generate All YT Shapes*)
 
 
-(* ::Input::Initialization:: *)
-ElementRepeat[m_,n_Integer?Positive]:=Sequence@@ConstantArray[m,n]
+(* Generate All YT Shapes *)
+
+ElementRepeat[m_,n_Integer?Positive] := Sequence@@ConstantArray[m,n]
+
+GenerateAllYTShapes[rowmaxno_Integer?Positive,colmaxno_Integer?Positive] := 
+  ReverseSort[#] &/@ DeleteDuplicates[Subsets[Array[ElementRepeat[#,colmaxno]&,rowmaxno],colmaxno]];
 
 
-(* ::Input::Initialization:: *)
-GenerateAllYTShapes[rowmaxno_Integer?Positive,colmaxno_Integer?Positive]:=ReverseSort[#] &/@ DeleteDuplicates[Subsets[Array[ElementRepeat[#,colmaxno]&,rowmaxno],colmaxno]];
+
+(* Small Functions For Tying Rule *)
+
+YTPad[YTshape_List,rowmaxno_Integer?Positive,columnno_Integer?Positive] := 
+  Transpose[Array[ArrayPad[Table[1,YTshape[[#]]],{0,(rowmaxno-YTshape[[#]])}]&,columnno]];
+
+ArrayPadLeftRigid[list_List,length_Integer?Positive,padding_] := 
+  Piecewise[{{ArrayPad[list,{length-Length[list],0},padding],length>Length[list]},
+    {list,length==Length[list]},
+    {Table[padding,length],length<Length[list]}}];
+
+TieComboAll[YTshape_List,rowmaxno_Integer?Positive,columnno_Integer?Positive] := Module[
+  {tietype,tieendposition,tiemaxno,tieall,tiecomboall,out},
+  tietype = Permutations[ArrayPadLeftRigid[{1,1},columnno,0]];
+  tieendposition = Array[Max@Position[tietype[[#]],1]&,Length[tietype]];
+  tiemaxno = Array[YTshape[[tieendposition[[#]]]]&,Length[tieendposition]];
+  tieall = ArrayReshape[Array[ConstantArray[tietype[[#]],tiemaxno[[#]]]&,columnno],{Total[tiemaxno],columnno}];
+  tiecomboall = DeleteDuplicates[Subsets[tieall,rowmaxno]];
+  out = Array[ArrayReshape[tiecomboall[[#]],{rowmaxno,columnno},0] &,Length[tiecomboall]];
+  out
+ ]
+
+InArrayQ[list_,element_] := If[Length@Position[list,x_/;x==element]==0,False,True];
+
+NoSymTestAdjColumn[YTshape_List,YTremnant_List, col_Integer?Positive] := Module[
+  {nosymneed,nosymquota,out},
+  nosymneed = Count[YTremnant[[1;;YTshape[[col+1]],col;;(col+1)]],{0,1}];
+  nosymquota = Count[YTremnant[[(YTshape[[col+1]]+1);;YTshape[[col]],1]] ,1];
+  out = nosymneed>nosymquota;
+  out
+ ]
+
+NoSymTest[YTshape_List,YTremnant_List,columnno_Integer?Positive] := 
+  Or[##]& @@Array[NoSymTestAdjColumn[YTshape,YTremnant,#]&,(columnno-1)];
 
 
-(* ::Subsection:: *)
-(*Small Functions For Tying Rule*)
+
+(* Tying Rule Full Function *)
+
+TyingRuleNoTie[YTshape_List] := {YTshape};
+
+TyingRuleTwoColumn[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {YTstruct,tiemaxno,tiecomboall,YTremnant,out},
+  YTstruct = YTPad[YTshape,rowmaxno,2];
+  tiemaxno = Min[YTshape];
+  tiecomboall = Array[ArrayReshape[Table[{1,1},#],{rowmaxno,2}]&,(tiemaxno+1),0];
+  YTremnant = Array[YTstruct - tiecomboall[[#]]&,Length[tiecomboall]];
+  out = Array[Total[YTremnant[[#]],{1}]&,Length[YTremnant]];
+  out
+ ]
+
+TyingRuleThreeColumn[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {YTstruct,tiecomboall,YTremnant,tieoutsideQ,YTremnantshape,irregularQ,violatesymQ,out},
+  YTstruct = YTPad[YTshape,rowmaxno,3];
+  tiecomboall = TieComboAll[YTshape,rowmaxno,3];
+  YTremnant = Array[YTstruct - tiecomboall[[#]]&,Length[tiecomboall]];
+  tieoutsideQ = Array[InArrayQ[YTremnant[[#]],-1]&,Length[YTremnant]];
+  YTremnant = Delete[YTremnant,Position[tieoutsideQ,True]];
+  YTremnantshape = Array[Total[YTremnant[[#]],{1}]&,Length[YTremnant]];
+  irregularQ = Array[!OrderedQ[YTremnantshape[[#]],GreaterEqual] &,Length[YTremnantshape]];
+  YTremnant = Delete[YTremnant,Position[irregularQ,True]];
+  violatesymQ = Array[NoSymTest[YTshape,YTremnant[[#]],3]&,Length[YTremnant]];
+  YTremnant =  Delete[YTremnant,Position[violatesymQ,True]];
+  out = Array[Total[YTremnant[[#]],{1}]&,Length[YTremnant]];
+  out
+ ]
+
+TyingRule[YTshaperaw_List,rowmaxno_Integer?Positive] := Module[
+  {columnno,YTshape,tiecomboall,YTremnant,tieoutsideQ,YTremnantshape,irregularQ,violatesymQ,out},
+  If[rowmaxno<Max[YTshaperaw],Abort[]];
+  If[!OrderedQ[YTshaperaw,GreaterEqual],Abort[]];
+  YTshape = DeleteCases[YTshaperaw,0];
+  columnno = Length[YTshape];
+  out = Piecewise[{{TyingRuleNoTie[YTshape],columnno<=1},
+  	{TyingRuleTwoColumn[YTshape,rowmaxno],columnno==2},
+  	{TyingRuleThreeColumn[YTshape,rowmaxno],columnno==3},
+  	{Abort[],columnno>=4}}];
+  out
+ ]
 
 
-(* ::Input::Initialization:: *)
-YTPad[YTshape_List,rowmaxno_Integer?Positive,columnno_Integer?Positive]:= Transpose[Array[ArrayPad[Table[1,YTshape[[#]]],{0,(rowmaxno-YTshape[[#]])}]&,columnno]];
+
+(* SU(n) to SO(n) Projection Matrix Input *)
+
+ProjAn1Bn[n_] := Module[
+  {out},
+  If[n>=3,
+    out = Table[KroneckerDelta[i,j]+KroneckerDelta[2n+1-i,j],{i,1,n},{j,1,2n}];
+    out[[n,n]]=2;
+    out[[n,n+1]]=2,
+    Abort[]];
+  out
+ ]
+
+ProjAn1Dn[n_] := Module[
+  {out},
+  If[n>=4,
+    out = Table[KroneckerDelta[i,j]+KroneckerDelta[2n-i,j],{i,1,n},{j,1,2n-1}];
+    out[[n,n-1]]=1;
+    out[[n,n+1]]=1,
+    Abort[]];
+  out
+ ]
 
 
-(* ::Input::Initialization:: *)
-ArrayPadLeftRigid[list_List,length_Integer?Positive,padding_] :=  Piecewise[{{ArrayPad[list,{length-Length[list],0},padding],length>Length[list]},{list,length==Length[list]},{Table[padding,length],length<Length[list]}}];
+
+(* Convert YTs to Dynkin Labels *)
+
+BYTToDynkinAn[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {out},
+  If[rowmaxno<Max[YTshape],Abort[]];
+  If[!OrderedQ[YTshape,GreaterEqual],Abort[]];
+  out = Array[Count[YTshape,#]&,rowmaxno];
+  out
+ ]
+
+BYTToDynkinBn[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {dynkinAn,out},
+  If[rowmaxno<3,Abort[]];
+  dynkinAn = BYTToDynkinAn[YTshape,rowmaxno];
+  out = ProjAn1Bn[rowmaxno][[All,1;;rowmaxno]].dynkinAn;
+  out
+ ]
+
+BYTToDynkinDn[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {dynkinAn,proj,conjugate,out},
+  If[rowmaxno<4,Abort[]];
+  dynkinAn = BYTToDynkinAn[YTshape,rowmaxno];
+  proj = ProjAn1Dn[rowmaxno][[All,1;;rowmaxno]].dynkinAn;
+  If[proj[[rowmaxno-1]]==proj[[rowmaxno]],
+    out = proj,
+    conjugate = proj;
+    conjugate[[rowmaxno]]=proj[[rowmaxno-1]];
+    conjugate[[rowmaxno-1]]=proj[[rowmaxno]];
+    out=Sequence[proj,conjugate]
+  ];
+  out
+ ]
 
 
-(* ::Input::Initialization:: *)
-TieComboAll[YTshape_List,rowmaxno_Integer?Positive,columnno_Integer?Positive]:=Module[
-{tietype,tieendposition,tiemaxno,tieall,tiecomboall,out},
-tietype = Permutations[ArrayPadLeftRigid[{1,1},columnno,0]];
-tieendposition = Array[Max@Position[tietype[[#]],1]&,Length[tietype]];
-tiemaxno = Array[YTshape[[tieendposition[[#]]]]&,Length[tieendposition]];
-tieall = ArrayReshape[Array[ConstantArray[tietype[[#]],tiemaxno[[#]]]&,columnno],{Total[tiemaxno],columnno}];
-tiecomboall = DeleteDuplicates[Subsets[tieall,rowmaxno]];
-out = Array[ArrayReshape[tiecomboall[[#]],{rowmaxno,columnno},0] &,Length[tiecomboall]];
-out
-]
+
+(* Convert Dynkin Labels to Irreps *)
+
+DynkinAnToIrrep[dynkin_List] := Irrep[A][##]& @@ dynkin;
+
+DynkinBnToIrrep[dynkin_List] := Irrep[B][##]& @@ dynkin;
+
+DynkinDnToIrrep[dynkin_List] := Irrep[D][##]& @@ dynkin;
 
 
-(* ::Input::Initialization:: *)
-InArrayQ[list_,element_]:=If[Length@Position[list,x_/;x==element]==0,False,True];
+
+(* Tying Rule Display YTs, Dynkin Labels and Irreps *)
+
+TyingDisplayYT[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {tying,tyingdynkin,out},
+  tying = TyingRule[YTshape,rowmaxno];
+  tyingdynkin = Array[BYTToDynkinAn[tying[[#]],rowmaxno]&,Length[tying]];
+  out = Subscript[YoungTableau[Irrep[A][##]]&@@#,"IR"]&/@tyingdynkin;
+  out
+ ]
+
+TyingDisplayAllB[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {tying,YTdisplay,dynkin,irrep,out},
+  tying = TyingRule[YTshape,rowmaxno];
+  YTdisplay = Subscript[YoungTableau[Irrep[A][##]]&@@#,"IR"]&/@Array[BYTToDynkinAn[tying[[#]],rowmaxno]&,Length[tying]];
+  dynkin = Array[BYTToDynkinBn[tying[[#]],rowmaxno]&,Length[tying]];
+  irrep = Array[DynkinBnToIrrep[dynkin[[#]]]&,Length[dynkin]];
+  out = Transpose[Array[{YTdisplay[[#]],dynkin[[#]],irrep[[#]]}&,Length[tying]]];
+  out
+ ]
+
+TyingDisplayAllD[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {tying,YTdisplay,dynkin,irrep,out},
+  tying = TyingRule[YTshape,rowmaxno];
+  YTdisplay = Subscript[YoungTableau[Irrep[A][##]]&@@#,"IR"]&/@Array[BYTToDynkinAn[tying[[#]],rowmaxno]&,Length[tying]];
+  dynkin = Array[{BYTToDynkinDn[tying[[#]],rowmaxno]}&,Length[tying]];
+  irrep = Array[If[Length[dynkin[[#]]]==2,
+    {DynkinDnToIrrep[dynkin[[#]][[1]]],DynkinDnToIrrep[dynkin[[#]][[2]]]},
+    DynkinDnToIrrep[dynkin[[#]][[1]]]]&,Length[dynkin]];
+  out = Transpose[Array[{YTdisplay[[#]],dynkin[[#]],irrep[[#]]}&,Length[tying]]];
+  out
+ ]
 
 
-(* ::Input::Initialization:: *)
-NoSymTestAdjColumn[YTshape_List,YTremnant_List, col_Integer?Positive]:=Module[
-{nosymneed,nosymquota,out},
-nosymneed = Count[YTremnant[[1;;YTshape[[col+1]],col;;(col+1)]],{0,1}];
-nosymquota = Count[YTremnant[[(YTshape[[col+1]]+1);;YTshape[[col]],1]] ,1];
-out = nosymneed>nosymquota;
-out
-]
 
+(* SU(n) to SO(n) Branching Rule Dimension Checks *)
 
-(* ::Input::Initialization:: *)
-NoSymTest[YTshape_List,YTremnant_List,columnno_Integer?Positive]:=Or[##]& @@Array[NoSymTestAdjColumn[YTshape,YTremnant,#]&,(columnno-1)];
+BranchingA2nBnDimCheck[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {tying,dynkin,irrep,dimBn,dynkinAn,dimAn,out},
+  tying = TyingRule[YTshape,rowmaxno];
+  dynkin = Array[BYTToDynkinBn[tying[[#]],rowmaxno]&,Length[tying]];
+  irrep = Array[DynkinBnToIrrep[dynkin[[#]]]&,Length[dynkin]];
+  dimBn = Total[Dim[irrep]];
+  dynkinAn = BYTToDynkinAn[YTshape,2rowmaxno];
+  dimAn = Dim[DynkinAnToIrrep[dynkinAn]];
+  out = (dimBn==dimAn);
+  out
+ ]
 
-
-(* ::Subsection:: *)
-(*Tying Rule Full Function*)
-
-
-(* ::Input::Initialization:: *)
-TyingRuleNoTie[YTshape_List]:={YTshape};
-
-
-(* ::Input::Initialization:: *)
-TyingRuleTwoColumn[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{YTstruct,tiemaxno,tiecomboall,YTremnant,out},
-YTstruct = YTPad[YTshape,rowmaxno,2];
-tiemaxno = Min[YTshape];
-tiecomboall = Array[ArrayReshape[Table[{1,1},#],{rowmaxno,2}]&,(tiemaxno+1),0];
-YTremnant = Array[YTstruct - tiecomboall[[#]]&,Length[tiecomboall]];
-out = Array[Total[YTremnant[[#]],{1}]&,Length[YTremnant]];
-out
-]
-
-
-(* ::Input::Initialization:: *)
-TyingRuleThreeColumn[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{YTstruct,tiecomboall,YTremnant,tieoutsideQ,YTremnantshape,irregularQ,violatesymQ,out},
-YTstruct = YTPad[YTshape,rowmaxno,3];
-tiecomboall = TieComboAll[YTshape,rowmaxno,3];
-YTremnant = Array[YTstruct - tiecomboall[[#]]&,Length[tiecomboall]];
-tieoutsideQ = Array[InArrayQ[YTremnant[[#]],-1]&,Length[YTremnant]];
-YTremnant = Delete[YTremnant,Position[tieoutsideQ,True]];
-YTremnantshape = Array[Total[YTremnant[[#]],{1}]&,Length[YTremnant]];
-irregularQ = Array[!OrderedQ[YTremnantshape[[#]],GreaterEqual] &,Length[YTremnantshape]];
-YTremnant = Delete[YTremnant,Position[irregularQ,True]];
-violatesymQ = Array[NoSymTest[YTshape,YTremnant[[#]],3]&,Length[YTremnant]];
-YTremnant =  Delete[YTremnant,Position[violatesymQ,True]];
-out = Array[Total[YTremnant[[#]],{1}]&,Length[YTremnant]];
-out
-]
-
-
-(* ::Input::Initialization:: *)
-TyingRule[YTshaperaw_List,rowmaxno_Integer?Positive]:=Module[
-{columnno,YTshape,tiecomboall,YTremnant,tieoutsideQ,YTremnantshape,irregularQ,violatesymQ,out},
-If[rowmaxno<Max[YTshaperaw],Abort[]];
-If[!OrderedQ[YTshaperaw,GreaterEqual],Abort[]];
-YTshape = DeleteCases[YTshaperaw,0];
-columnno = Length[YTshape];
-out = Piecewise[{{TyingRuleNoTie[YTshape],columnno<=1},{TyingRuleTwoColumn[YTshape,rowmaxno],columnno==2},{TyingRuleThreeColumn[YTshape,rowmaxno],columnno==3},{Abort[],columnno>=4}}];
-out
-]
-
-
-(* ::Subsection:: *)
-(*SU(n) to SO(n) Projection Matrix Input*)
-
-
-(* ::Input::Initialization:: *)
-ProjAn1Bn[n_]:=Module[
-{out},
-If[n>=3,
-out = Table[KroneckerDelta[i,j]+KroneckerDelta[2n+1-i,j],{i,1,n},{j,1,2n}];
-out[[n,n]]=2;
-out[[n,n+1]]=2,
-Abort[]];
-out
-]
-
-
-(* ::Input::Initialization:: *)
-ProjAn1Dn[n_]:=Module[
-{out},
-If[n>=4,
-out = Table[KroneckerDelta[i,j]+KroneckerDelta[2n-i,j],{i,1,n},{j,1,2n-1}];
-out[[n,n-1]]=1;
-out[[n,n+1]]=1,
-Abort[]];
-out
-]
-
-
-(* ::Subsection:: *)
-(*Convert YTs to Dynkin Labels*)
-
-
-(* ::Input::Initialization:: *)
-BYTToDynkinAn[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{out},
-If[rowmaxno<Max[YTshape],Abort[]];
-If[!OrderedQ[YTshape,GreaterEqual],Abort[]];
-out = Array[Count[YTshape,#]&,rowmaxno];
-out
-]
-
-
-(* ::Input::Initialization:: *)
-BYTToDynkinBn[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{dynkinAn,out},
-If[rowmaxno<3,Abort[]];
-dynkinAn = BYTToDynkinAn[YTshape,rowmaxno];
-out = ProjAn1Bn[rowmaxno][[All,1;;rowmaxno]].dynkinAn;
-out
-]
-
-
-(* ::Input::Initialization:: *)
-BYTToDynkinDn[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{dynkinAn,proj,conjugate,out},
-If[rowmaxno<4,Abort[]];
-dynkinAn = BYTToDynkinAn[YTshape,rowmaxno];
-proj = ProjAn1Dn[rowmaxno][[All,1;;rowmaxno]].dynkinAn;
-If[proj[[rowmaxno-1]]==proj[[rowmaxno]],
-out = proj,
-conjugate = proj;
-conjugate[[rowmaxno]]=proj[[rowmaxno-1]];
-conjugate[[rowmaxno-1]]=proj[[rowmaxno]];
-out=Sequence[proj,conjugate]
-];
-out
-]
-
-
-(* ::Subsection:: *)
-(*Convert Dynkin Labels to Irreps*)
-
-
-(* ::Input::Initialization:: *)
-DynkinAnToIrrep[dynkin_List]:= Irrep[A][##]& @@ dynkin;
-
-
-(* ::Input::Initialization:: *)
-DynkinBnToIrrep[dynkin_List]:= Irrep[B][##]& @@ dynkin;
-
-
-(* ::Input::Initialization:: *)
-DynkinDnToIrrep[dynkin_List]:= Irrep[D][##]& @@ dynkin;
-
-
-(* ::Subsection:: *)
-(*Tying Rule Display YTs, Dynkin Labels and Irreps*)
-
-
-(* ::Input::Initialization:: *)
-TyingDisplayAllB[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{tying,YTdisplay,dynkin,irrep,out},
-tying = TyingRule[YTshape,rowmaxno];
-YTdisplay = Subscript[YoungTableau[Irrep[A][##]]&@@#,"IR"]&/@Array[BYTToDynkinAn[tying[[#]],rowmaxno]&,Length[tying]];
-dynkin = Array[BYTToDynkinBn[tying[[#]],rowmaxno]&,Length[tying]];
-irrep = Array[DynkinBnToIrrep[dynkin[[#]]]&,Length[dynkin]];
-out = Transpose[Array[{YTdisplay[[#]],dynkin[[#]],irrep[[#]]}&,Length[tying]]];
-out
-]
-
-
-(* ::Input::Initialization:: *)
-TyingDisplayAllD[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{tying,YTdisplay,dynkin,irrep,out},
-tying = TyingRule[YTshape,rowmaxno];
-YTdisplay = Subscript[YoungTableau[Irrep[A][##]]&@@#,"IR"]&/@Array[BYTToDynkinAn[tying[[#]],rowmaxno]&,Length[tying]];
-dynkin = Array[{BYTToDynkinDn[tying[[#]],rowmaxno]}&,Length[tying]];
-irrep = Array[If[Length[dynkin[[#]]]==2,{DynkinDnToIrrep[dynkin[[#]][[1]]],DynkinDnToIrrep[dynkin[[#]][[2]]]},DynkinDnToIrrep[dynkin[[#]][[1]]]]&,Length[dynkin]];
-out = Transpose[Array[{YTdisplay[[#]],dynkin[[#]],irrep[[#]]}&,Length[tying]]];
-out
-]
-
-
-(* ::Subsection:: *)
-(*SU(n) to SO(n) Branching Rule Dimension Checks*)
-
-
-(* ::Input::Initialization:: *)
-BranchingA2nBnDimCheck[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{tying,dynkin,irrep,dimBn,dynkinAn,dimAn,out},
-tying = TyingRule[YTshape,rowmaxno];
-dynkin = Array[BYTToDynkinBn[tying[[#]],rowmaxno]&,Length[tying]];
-irrep = Array[DynkinBnToIrrep[dynkin[[#]]]&,Length[dynkin]];
-dimBn = Total[Dim[irrep]];
-dynkinAn = BYTToDynkinAn[YTshape,2rowmaxno];
-dimAn = Dim[DynkinAnToIrrep[dynkinAn]];
-out = (dimBn==dimAn);
-out
-]
-
-
-(* ::Input::Initialization:: *)
-BranchingA2n1DnDimCheck[YTshape_List,rowmaxno_Integer?Positive]:=Module[
-{tying,dynkin,irrep,dimDn,dynkinAn,dimAn,out},
-tying = TyingRule[YTshape,rowmaxno];
-dynkin = Array[BYTToDynkinDn[tying[[#]],rowmaxno]&,Length[tying]];
-irrep = Array[DynkinDnToIrrep[dynkin[[#]]]&,Length[dynkin]];
-dimDn = Total[Dim[irrep]];
-dynkinAn = BYTToDynkinAn[YTshape,2rowmaxno-1];
-dimAn = Dim[DynkinAnToIrrep[dynkinAn]];
-out = (dimDn==dimAn);
-out
-]
+BranchingA2n1DnDimCheck[YTshape_List,rowmaxno_Integer?Positive] := Module[
+  {tying,dynkin,irrep,dimDn,dynkinAn,dimAn,out},
+  tying = TyingRule[YTshape,rowmaxno];
+  dynkin = Array[BYTToDynkinDn[tying[[#]],rowmaxno]&,Length[tying]];
+  irrep = Array[DynkinDnToIrrep[dynkin[[#]]]&,Length[dynkin]];
+  dimDn = Total[Dim[irrep]];
+  dynkinAn = BYTToDynkinAn[YTshape,2rowmaxno-1];
+  dimAn = Dim[DynkinAnToIrrep[dynkinAn]];
+  out = (dimDn==dimAn);
+  out
+ ]
